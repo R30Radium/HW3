663098fa9f684b6bcbeb55e59acca97e99f8c9a7
R30Radium <R30Radium@gmail.ru>
2022-09-24 23:57:31 +0400

    Дз 4.3 исправленный

--- src/main/resources/liquibase/scripts/course-four-lesson-three.sql
+++ src/main/resources/liquibase/scripts/course-four-lesson-three.sql
@@ -1,5 +1,10 @@
+-- liquibase formatted sql
+
+-- changeSet: hoggwarts:1
 
 CREATE INDEX student_name_index on student (name);
+
+-- changeSet: hoggwarts: 2
 
 CREATE INDEX faculty_name_index on faculty (name);
 

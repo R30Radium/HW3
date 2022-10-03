68ed57f27c8f59def65bb8c65d19f2dd65b70730
R30Radium <R30Radium@gmail.ru>
2022-10-03 21:09:59 +0400

    Дз 4.5 qwer

--- src/main/java/ru/hogwarts/school/service/StudentService.java
+++ src/main/java/ru/hogwarts/school/service/StudentService.java
@@ -14,7 +14,7 @@ import java.util.stream.Stream;
 @Service
 public class StudentService {
 
-    Logger logger = LoggerFactory.getLogger(StudentService.class);
+   private final Logger logger = LoggerFactory.getLogger(StudentService.class);
 
     private StudentRepository studentRepository;
 
